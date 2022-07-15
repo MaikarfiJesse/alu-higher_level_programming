c23422aaf17621750855dce3eb3694b2f21fa6ac
root <jessejeckx@gm>
2022-07-15 07:02:20 -0700

    Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [list(map((lambda x: x * x), elm)) for elm in matrix]
+    if not matrix:
+        print()
+    return [[item**2 for item in row] for row in matrix]
