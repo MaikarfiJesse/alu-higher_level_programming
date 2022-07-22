3970d2ed72860972b835f30773bdd846e465adac
root <jessejeckx@gm>
2022-07-22 05:43:26 -0700

    Stingly linked list

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -1,47 +1,76 @@
 #!/usr/bin/python3
+"""this is a module docstring?"""
 
 
 class Node:
-    """A node class"""
+    """define a node"""
 
     def __init__(self, data, next_node=None):
-        """Initialize class"""
+        """this is the init"""
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """To retrieve data"""
+        """gets data"""
         return self.__data
 
-    @size.setter
+    @data.setter
     def data(self, value):
-        """To set data"""
+        """sets data"""
         if not isinstance(value, int):
-            raise TypeError("data must be an integer")
+            raise TypeError('data must be an integer')
         self.__data = value
 
     @property
     def next_node(self):
-        """To retrieve data"""
+        """get the next node for this node
+        Returns:
+            the next node
+        """
         return self.__next_node
 
-    @size.setter
+    @next_node.setter
     def next_node(self, value):
-        """To set data"""
-        if not isinstance(value, None):
-            raise TypeError("next_node must be a Node object")
+        """sets node"""
+        if value is not None and type(value) != Node:
+            raise TypeError('next_node must be a Node object')
         self.__next_node = value
 
 
 class SinglyLinkedList:
-    """A SinglyLinkedList class"""
+    """define a sll"""
 
     def __init__(self):
-        """Initialize class"""
-        self.__head = 0
+        """this is init"""
+        self.head = None
+
+    def __str__(self):
+        """teach python to print my way
+        Returns: the printed thing"""
+        printsll = ""
+        location = self.head
+        while location:
+            printsll += str(location.data) + "\n"
+            location = location.next_node
+        return printsll[:-1]
 
     def sorted_insert(self, value):
-        """Inserts a new Node into the correct sorted position in the list
-        (increasing order)"""
-        self.__sorted_insert = value
+        """insert in a sorted fashion
+        Args:
+            value: what the value will be on the node
+        """
+        new = Node(value)
+        if not self.head:
+            self.head = new
+            return
+        if value < self.head.data:
+            new.next_node = self.head
+            self.head = new
+            return
+        location = self.head
+        while location.next_node and location.next_node.data < value:
+            location = location.next_node
+        if location.next_node:
+            new.next_node = location.next_node
+        location.next_node = new
