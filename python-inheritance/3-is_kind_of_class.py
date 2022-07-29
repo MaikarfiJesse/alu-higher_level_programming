abbb5de5e96cb6fd6f4166e756532f840187f78a
root <jessejeckx@gm>
2022-07-29 14:35:58 -0700

    Same class or inherit from

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""
-Contains the is_kind_of_class function
-"""
+"""is kind of class module - no test cases needed"""
 
 
 def is_kind_of_class(obj, a_class):
-    """True if obj is an instance or inherited from a_class, else False"""
-    return (isinstance(obj, a_class))
-
+    """checks if obj is of a sublass of a_class
+    Args:
+        obj: the object
+        a_class: the class we are checking it for
+    """
+    return isinstance(obj, a_class)
