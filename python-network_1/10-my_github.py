6d1158f6b17ab02e4cd3fcf9a97d750ec5a4a0f7
root <jessejeckx@gm>
2022-08-05 12:01:01 -0700

    My Github!

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-""" get user id
-    from github
-"""
-import sys
+""" takes your Github credentials (username and password) and uses the Github
+API to display your id"""
 import requests
-
-
-url = 'https://api.github.com/user'
+from sys import argv
 
 if __name__ == "__main__":
-    res = requests.get(url, auth=(sys.argv[1], sys.argv[2])).json()
-    if 'id' in res:
-        print(res['id'])
-    else:
-        print(None)
+    url = "https://api.github.com/user"
+    auth = (argv[1], argv[2])
+    response = requests.get(url, auth=auth)
+    try:
+        print(response.json().get("id"))
+    except ValueError:
+        print("Not a valid JSON")
